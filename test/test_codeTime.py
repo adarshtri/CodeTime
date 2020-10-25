e3d953b4dc7e1f2f3e6fbff1e139a917f427395e
Prithvi Patel <pri281097@gmail.com>
2020-10-25 13:54:35 -0400

    Fix Flake8 errors

--- test/test_codeTime.py
+++ test/test_codeTime.py
@@ -1,6 +1,5 @@
 import sublime
 import sys
-import os
 
 from datetime import datetime as dt
 from unittest import TestCase
@@ -12,17 +11,17 @@ codeTime = sys.modules["SE_Fall20_Project-1.codeTime"]
 
 class TestFunctions(TestCase):
 
-	@patch('time.time', return_value=100)
-	def test_when_activated(self, mock_time):
-		view = Mock()
-		view.filename.return_value = "sample.txt"
-		datetime = Mock()
-		codeTime.when_activated(view)
-		view.window.assert_called_once()
+    @patch('time.time', return_value=100)
+    def test_when_activated(self, mock_time):
+        view = Mock()
+        view.filename.return_value = "sample.txt"
+        # datetime = Mock()
+        codeTime.when_activated(view)
+        view.window.assert_called_once()
 
-	def test_when_deactivated(self):
-		view = Mock()		
-		view.file_name.return_value = "sample.txt"
-		curr_date = dt.now().strftime('%Y-%m-%d')
-		codeTime.file_times_dict[curr_date] = {'sample.txt':["1234", None]}
-		view.assert_called_once()
+    def test_when_deactivated(self):
+        view = Mock()
+        view.file_name.return_value = "sample.txt"
+        curr_date = dt.now().strftime('%Y-%m-%d')
+        codeTime.file_times_dict[curr_date] = {'sample.txt': ["1234", None]}
+        view.assert_called_once()
