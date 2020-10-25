e65807df1b9540ec8efd2b35423caefd544395b8
Prithvi Patel <pri281097@gmail.com>
2020-10-25 15:47:38 -0400

    Fix periodicLogSaver test

--- test/test_periodicLogSaver.py
+++ test/test_periodicLogSaver.py
@@ -14,13 +14,12 @@ periodicLogSaver = sys.modules["CodeTime.code.SublimePlugin.periodicLogSaver"]
 class TestPeriodicLogSaver(TestCase):
 
     def test_write_log_file(self):
-        logger = periodicLogSaver.PeriodicLogSaver()
+        BASE_PATH = os.path.join(os.path.expanduser('~'), '.codeTime')
+        FILE_PATH = os.path.join(BASE_PATH, '.sublime_logs')
+        logger = periodicLogSaver.PeriodicLogSaver(kwargs={'LOG_FILE_PATH': FILE_PATH})
         try:
             d = {'2020-09-19': {'temp1.py': [[1000, 2000], [3000, 3200]]},
                 '2020-09-20': {'temp2.py': [[5000, 6000]]}}  # noqa: E128, E501
-
-            BASE_PATH = os.path.join(os.path.expanduser('~'), '.codeTime')
-            FILE_PATH = os.path.join(BASE_PATH, '.sublime_logs')
 
             _ = logger.write_log_file(d)  # noqa: F841
 
